cdcba542e11071d48f10665f35d8c923cc136047
joao-freitas-dev <joao.freitasraf@gmail.com>
2023-11-09 20:09:22 -0300

    fix- name propriety destination

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -1,11 +1,12 @@
 const launches = new Map();
+let lastFlight = 100;
 
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
-  LaunchDate: new Date("Dezembro 27, 2030"),
-  destionation: "Kepler-442 b",
+  launchDate: new Date("Dezembro 27, 2030"),
+  destination: "Kepler-442 b",
   customer: ["Nasa", "MSZ"],
   upcoming: true,
   success: true,
@@ -17,6 +18,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  lastFlight++;
+  launches.set(
+    lastFlight,
+    Object.assign(launch, {
+      customer: ["Nasa", "Jotape"],
+      success: true,
+      upcoming: true,
+      flightNumber: lastFlight,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
