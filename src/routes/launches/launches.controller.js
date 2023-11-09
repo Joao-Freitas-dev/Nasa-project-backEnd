42c62b69ce4e71df3e639fe194da35fffa55d439
joao-freitas-dev <joao.freitasraf@gmail.com>
2023-11-09 20:12:20 -0300

    validation data -launch

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,8 +1,33 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
+
+function httpaddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res
+      .status(400)
+      .json({ error: "campos obrigatórios ausentes e/ou incorretos" });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "O campo Data está incorreto, por favor digite uma data válida",
+    });
+  }
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
 module.exports = {
   httpGetAllLaunches,
+  httpaddNewLaunch,
 };
